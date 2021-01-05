570f7021917197567fd445eee3404368cd2b9950
Giovanny Fialho <giovannyfialho@MacBook-Air-de-Giovanny.local>
2021-01-05 20:38:45 -0300

    criacao de toast messages

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import React from 'react'
 import GlobalStyle from './styles/global'
 import Sigin from './Pages/Sigin'
 
-import { AuthProvider } from './hooks/AuthContext'
+import AppProvider from './hooks'
 
 const App: React.FC = () => {
     return (
         <>
-            <AuthProvider>
+            <AppProvider>
                 <Sigin />
-            </AuthProvider>
+            </AppProvider>
+
             <GlobalStyle />
         </>
     );
--- src/Pages/Sigin/index.tsx
+++ src/Pages/Sigin/index.tsx
@@ -4,7 +4,9 @@ import { FormHandles } from '@unform/core'
 import { Form } from '@unform/web'
 import * as Yup from 'yup'
 
-import { useAuth } from '../../hooks/AuthContext'
+import { useAuth } from '../../hooks/auth'
+import { useToast } from '../../hooks/toast'
+
 import getValidationErrors from '../../utils/getValidationErrors'
 
 import logoSvg from '../../assets/logo.svg'
@@ -23,6 +25,7 @@ const Sigin: React.FC = () => {
     const formRef = useRef<FormHandles>(null)
 
     const { signIn } = useAuth()
+    const { addToast } = useToast()
 
     const handleSubmit = useCallback(async (data: SignInFormData) => {
         try{
@@ -37,16 +40,24 @@ const Sigin: React.FC = () => {
                 abortEarly: false
             })
 
-            signIn({
+            await signIn({
                 email: data.email,
                 password: data.password,
             })
         } catch (err) {
-            const errors = getValidationErrors(err)
+            if (err instanceof Yup.ValidationError) {
+                const errors = getValidationErrors(err)
 
-            formRef.current?.setErrors(errors)
+                formRef.current?.setErrors(errors)
+            }
+
+            addToast({
+                type: 'error',
+                title: 'Erro na autenticação',
+                description: 'Ocorreu um erro ao fazer login, cheque as credenciais.'
+            })
         }
-    }, [signIn])
+    }, [signIn, addToast])
 
     return (
         <Container>
